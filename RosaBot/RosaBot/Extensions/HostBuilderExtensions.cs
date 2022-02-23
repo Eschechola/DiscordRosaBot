11edbb0a455cfbf65c7ebfd9eac19ddae4e48b59
Lucas Eschechola <lucas.eschechola@outlook.com>
2022-02-22 23:00:10 -0300

    fix/create-host-builder: Add HostBuilder to Program

---
+++ RosaBot/RosaBot/Extensions/HostBuilderExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace RosaBot.Application.Extensions
+{
+    public static class HostBuilderExtensions
+    {
+        private const string ConfigureServicesMethodName = "ConfigureServices";
+        public static IHostBuilder UseStartup<TStartup>(this IHostBuilder hostBuilder)
+            where TStartup : class
+        {
+            hostBuilder.ConfigureServices((context, serviceCollection) =>
+            {
+                var cfgServicesMethod = typeof(TStartup).GetMethod(
+                    ConfigureServicesMethodName,
+                    new Type[] { typeof(IServiceCollection) });
+
+                var hasConfigCtor = typeof(TStartup).GetConstructor(
+                    new Type[] { typeof(IConfiguration) }) != null;
+
+                var startUpObj = hasConfigCtor ?
+                    (TStartup)Activator.CreateInstance(typeof(TStartup), context.Configuration) :
+                    (TStartup)Activator.CreateInstance(typeof(TStartup), null);
+
+                cfgServicesMethod?.Invoke(startUpObj, new object[] { serviceCollection });
+            });
+
+            // chain the response
+            return hostBuilder;
+        }
+    }
+}
